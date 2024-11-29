71edd8f88163108f6e67e6415f5f46816a70a6d3
buttha <hannanalibutt10@gmail.com>
2024-11-29 08:27:12 +0100

    Klassen

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,6 @@
 public class Client {
+    public static void main(String[] args) {
+        TCPIPConnection tcpipc = new TCPIPConnection("alpha.mike-r.com",17);
+        tcpipc.servernachrichtAusgeben();
+    }
 }
